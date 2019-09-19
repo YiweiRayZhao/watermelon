6887b3ed1a1a2e5407ed16c563bd4453384f98a2
Weixin Tang <weixtang@cs.stonybrook.edu>
2019-09-18 20:32:41 -0400

    add buttom hover

---
+++ Game_Document/JS/sidnav.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// var Login_Button = document.getElementById("Login");
+//
+// Login_Button.onclick = function(){
+//
+//     openNav();
+//
+// }
+
